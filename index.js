ce0482fa0b5ede219ff3b7c0047fece86e67e78f
Chris Morda <chris@chrismorda.com>
2017-04-05 22:20:07 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var http = require('http');
+fs = require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode) {
+    if (!responseCode) responseCode = 200;
+    fs.readFile(__dirname + path, function(err, data) {
+        if (err) {
+            res.writeHead(500, {
+                'Content-Type': 'text/plain'
+            });
+            res.end('500 - Internal Error');
+        } else {
+            res.writeHead(responseCode, {
+                'Content-Type': contentType
+            });
+            res.end(data);
+        }
+    });
+}
+
+http.createServer(function(req, res) {
+    // normalize url by removing querystring, optional
+    // trailing slash, and making it lowercase
+    var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+    
+    switch (path) {
+       case '':
+	   		serveStaticFile(res, '/home.html', 'text/html'); break;
+            break;
+        
+        case '/about.html':
+			serveStaticFile(res, '/public/about.html', 'text/html'); 
+			break;
+        
+        default:
+            res.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
+            res.end('Not Found');
+            break;
+    }
+}).listen(3000);
+
+console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
